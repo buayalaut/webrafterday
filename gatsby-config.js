342a859278620f4c8887af117878b117b9fa1f0c
buayalaut <45431354+buayalaut@users.noreply.github.com>
2021-01-21 19:04:04 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `The Plain Gatsby`,
-    description: `A simple minimalist starter for your personal blog.`,
-    author: `@wangonya`,
-    avatar: `https://avatars0.githubusercontent.com/u/19375569?s=460&v=4`,
-    siteUrl: `https://the-plain-gatsby.netlify.com`,
+    title: `Rafterday.net - Sebuah Catatan`,
+    description: `-`,
+    author: `@buayalaut`,
+    avatar: `#`,
+    siteUrl: `https://rafterday.net`,
   },
   plugins: [
     {
